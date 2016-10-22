e5b7d16c3615d786ca243100c23120c2e437fb85
James Edwards <jameshedwards@protonmail.com>
2016-10-21 22:24:02 -0400

    add more functionality

--- flood-game.py
+++ flood-game.py
@@ -8,19 +8,31 @@ BOXSIZE = 40
 BOARDWIDTH = WIDTH//BOXSIZE
 BOARDHEIGHT = HEIGHT//BOXSIZE
 board = []
-BLACK = (  0,  0,  0)
+boardColors = []
+WHITE = (255,255,255)
 RED   = (255,  0,  0)
 GREEN = (  0,255,  0)
 ORANGE= (255,128,  0)
 BLUE  = (  0,  0,255)
 GRAY  = (100,100,100)
 PURPLE= (255,  0,255)
+colors = [RED,GREEN,ORANGE,BLUE,GRAY,PURPLE]
+windowSurface = pygame.display.set_mode((WIDTH,HEIGHT), 0, 32)
+
+class Box(object):
+
+    def __init__(self, color, value, checked=False):
+        self.color = color
+        self.value = value
+        self.checked = checked
+
+
 
 def main():
-    windowSurface = pygame.display.set_mode((WIDTH,HEIGHT), 0, 32)
     pygame.display.set_caption("Hello world!")
 
     initializeBoard()
+    drawBoard()
     pygame.display.update()
     
     while True:
@@ -29,20 +41,44 @@ def main():
                 pygame.quit()
                 sys.exit()
 	    elif e.type == MOUSEBUTTONUP:
-	        mousex, mousey = event.pos
-	        box = getBox(mousex,mousey)
-		updateColors(box)
+	        mousex, mousey = e.pos
+	        boxy, boxx = getBox(mousex,mousey)
+		updateColors(boxy,boxx)
+                drawBoard()
                 pygame.display.update()
 
 
 def initializeBoard():
-    pass
+    for j in range(BOARDHEIGHT):
+        row = []
+        rowC = []
+        for i in range(BOARDWIDTH):
+            randCi = random.randint(0,len(colors)-1)
+            row.append(Box(colors[randCi],False))
+        board.append(row)
+
+    board[0][0].value = True
+
+def drawBoard():
+    windowSurface.fill(WHITE)
+    for j in range(BOARDHEIGHT):
+        for i in range(BOARDWIDTH):
+            rr = (i*BOXSIZE,j*BOXSIZE,BOXSIZE,BOXSIZE)
+            pygame.draw.rect(windowSurface,board[j][i].color,rr)
+
 
 def getBox(x,y):
-    pass
+    boxx = x//BOXSIZE
+    boxy = y//BOXSIZE
+    return boxy,boxx
 
-def updateColors(box):
-    pass
+
+def updateColors(boxy, boxx):
+    if board[boxy][boxx].color == board[0][0].color:
+        return
+    newC = board[boxy][boxx].color
+
+
 
 
 if __name__ == '__main__':
